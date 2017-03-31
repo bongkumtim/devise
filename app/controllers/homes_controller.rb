eefd780af04061d1ad095dd108e4847bdd220564
Bong <mjbond_bong2@yahoo.com>
2017-03-31 20:31:30 +0800

    done

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,19 @@
 class HomesController < ApplicationController
 
 	def index
-		@ewallet = Ewallet.where(user_id: current_user.id)
-		@ringgit = @ewallet.find_by(currency: 'ringgitcoin')
+		@ewallet = Ewallet.where(user_id: current_user.id).order(created_at: :desc)
+		
+		ringgit = @ewallet.where(currency: 'ringgitcoin')
+		@ringgit = ringgit.sum(:balance)
+
+		sing = @ewallet.where(currency: 'singcoin')
+		@sing = sing.sum(:balance)
+
+		rupiah = @ewallet.where(currency: 'rupiahcoin')
+		@rupiah = rupiah.sum(:balance)
+
+		baht = @ewallet.where(currency: 'bahtcoin')
+		@baht = baht.sum(:balance)
 	end
 
 	def new
